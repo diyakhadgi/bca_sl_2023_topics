99b59c5e9c06d4cabf340c9012fdc26fa04ae34b
diyakhadgi <khadgidiya8@gmail.com>
2023-12-07 11:10:11 +0545

    array methods

--- day-four/js/script.js
+++ day-four/js/script.js
@@ -44,3 +44,39 @@ console.log(movies)
 
 movies.unshift("Hello")
 console.log(movies)
+
+// 5. splice() - add and /or remove element from specified index 
+let languages = ["Java", "Javascript", "Php"]
+
+// this method takes 3 arguments 
+// 1st - position 
+// 2nd - number elements want to remove
+// 3rd - elements can be more than one 
+
+// to add element - set 2nd argument 
+console.log(languages)
+languages.splice(1, 0, "python") // 0 doesnt remove anything
+
+// to remove element - set 2nd argument greater than 0 
+console.log(languages)
+languages.splice(1, 2, "dart") // 1 removes python from index 1 and adds dart
+console.log(languages)
+
+// 6. slice() - to break array into another piece
+console.log("Slice example")
+let province = ["Bagmati", "Koshi", "Sudur Paschim", "Karnali"]
+let province_two = province.slice(0,3) //index two samma
+console.log(province)
+console.log(province_two) 
+
+// 7. concat - to merge one or more array without mutating primary
+// array 
+let group_one = ["red", "blue"] 
+let group_two = ["green", "yellow"] 
+let groups = group_one.concat(group_two)
+console.log(groups)
+
+// 8.flat() - converts multi-dimensional array to single flat array
+let cities = [["KTM","POKHARA"],["BUTWAL","HETAUDA","JHAPA"]]
+let cities_nepal = cities.flat()
+console.log(cities)
